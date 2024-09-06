582b78ccb762a0d22f77e03ee00686a570da2f31
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-06 14:35:26 +0200

    feat: add describe fn

--- src/components/Greeting.test.jsx
+++ src/components/Greeting.test.jsx
@@ -1,14 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import Greetings from "./Greeting";
 
-test('renders Hello World as a text', () => {
-    // Arrange
-    render(<Greetings />);
+describe('Greeting component', () => {
+    test('renders Hello World as a text', () => {
+        // Arrange
+        render(<Greetings />);
+    
+        // Act
+        // ....
+    
+        // Assert
+        const helloWorldElement = screen.getByText('Hello World!');
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+})
 
-    // Act
-    // ....
-
-    // Assert
-    const helloWorldElement = screen.getByText('Hello World!');
-    expect(helloWorldElement).toBeInTheDocument();
-});
